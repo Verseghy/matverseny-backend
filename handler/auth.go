4146fccb8296c0b6b266013e4ec9db9b5378a727
TwoDCube <2dcube01@gmail.com>
2021-06-09 19:27:02 +0200

    Fix invalid hash password error

--- handler/auth.go
+++ handler/auth.go
@@ -50,6 +50,7 @@ func (h *authHandler) Login(ctx context.Context, req *pb.LoginRequest) (*pb.Logi
 	if err != nil {
 		if err == bcrypt.ErrMismatchedHashAndPassword {
 			log.Logger.Debug("invalid password", zap.Error(err))
+			return nil, err
 		}
 
 		return nil, errs.ErrCryptographic
