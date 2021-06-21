08ae7f787ce68fe0e507c375a6ceee7c8743c04d
TwoDCube <2dcube01@gmail.com>
2021-06-21 19:29:20 +0200

    Fix SetSolution setting nil ID

--- entity/solution.go
+++ entity/solution.go
@@ -3,7 +3,7 @@ package entity
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Solution struct {
-	ID        primitive.ObjectID `bson:"_id"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
 	Team      primitive.ObjectID `bson:"team"`
 	ProblemID primitive.ObjectID `bson:"problem_id"`
 	Value     int64              `bson:"value"`
