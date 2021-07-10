ed9101babcd85588413f90e37202ce8abcb71393
TwoDCube <2dcube01@gmail.com>
2021-07-10 18:29:10 +0200

    Change logging to json

--- log/log.go
+++ log/log.go
@@ -1,10 +1,38 @@
 package log
 
-import "go.uber.org/zap"
+import (
+	"encoding/json"
+	"go.uber.org/zap"
+)
 
 var Logger *zap.Logger
 
 func EnsureLogger() {
-	Logger, _ = zap.NewDevelopment()
-	defer Logger.Sync()
+	if Logger != nil {
+		return
+	}
+
+	rawJSON := []byte(`{
+	  "level": "debug",
+	  "encoding": "json",
+	  "outputPaths": ["stdout"],
+	  "errorOutputPaths": ["stderr"],
+	  "encoderConfig": {
+	    "messageKey": "message",
+	    "levelKey": "level",
+	    "levelEncoder": "lowercase"
+	  }
+	}`)
+
+	var cfg zap.Config
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		panic(err)
+	}
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	logger.Info("logger construction succeeded")
 }
