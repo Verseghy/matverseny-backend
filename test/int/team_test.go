e1a34143f67819333415e8213d433abba1fefb9f
Szepesi Tibor <sztibi023@gmail.com>
2021-10-12 10:07:12 +0200

    Add tests for team service (#15)
    * Add tests for join team
    
    * Add tests for leave team
    
    * Add tests for get team info
    
    * Add tests for update team
    
    * Add tests for disband team
    
    * Add tests for change lock
    
    * Add tests for change co-owner status
    
    * Add more tests the the Get Team Info

--- test/int/team_test.go
+++ test/int/team_test.go
@@ -76,6 +76,522 @@ var _ = Describe("Team", func() {
 		})
 	})
 
+	Describe("Join Team", func() {
+		var user1 User
+		var user2 User
+		var user3 User
+
+		var team1 Team
+		var team2 Team
+
+		BeforeEach(func() {
+			cleanupMongo()
+			user1 = registerUser(authClient, 0)
+			user2 = registerUser(authClient, 1)
+			user3 = registerUser(authClient, 2)
+
+			team1 = createTeam(user1, "Test team 1", teamClient)
+			team2 = createTeam(user2, "Test team 2", teamClient)
+		})
+
+		Specify("Should join team", func() {
+			_, err := teamClient.JoinTeam(user3.Context(), &pb.JoinTeamRequest{
+				Code: team1.JoinCode,
+			})
+
+			Expect(err).To(BeNil())
+		})
+
+		Specify("Can't join team twice", func() {
+			_, err := teamClient.JoinTeam(user3.Context(), &pb.JoinTeamRequest{
+				Code: team1.JoinCode,
+			})
+
+			Expect(err).To(BeNil())
+
+			_, err = teamClient.JoinTeam(user3.Context(), &pb.JoinTeamRequest{
+				Code: team1.JoinCode,
+			})
+
+			Expect(err).To(MatchBackendError(errs.ErrHasTeam))
+		})
+
+		Specify("Can't join to another team", func() {
+			_, err := teamClient.JoinTeam(user3.Context(), &pb.JoinTeamRequest{
+				Code: team1.JoinCode,
+			})
+
+			Expect(err).To(BeNil())
+
+			_, err = teamClient.JoinTeam(user3.Context(), &pb.JoinTeamRequest{
+				Code: team2.JoinCode,
+			})
+
+			Expect(err).To(MatchBackendError(errs.ErrHasTeam))
+		})
+
+		Specify("Wrong join code", func() {
+			_, err := teamClient.JoinTeam(user3.Context(), &pb.JoinTeamRequest{
+				Code: "0",
+			})
+
+			Expect(err).To(MatchBackendError(errs.ErrNotFound))
+		})
+	})
+
+	Describe("Leave Team", func() {
+		var user1 User
+		var user2 User
+
+		var team Team
+
+		BeforeEach(func() {
+			cleanupMongo()
+			user1 = registerUser(authClient, 0)
+			user2 = registerUser(authClient, 1)
+
+			team = createTeam(user1, "Test team 1", teamClient)
+		})
+
+		Specify("Can't leave without a team", func() {
+			_, err := teamClient.LeaveTeam(user2.Context(), &pb.LeaveTeamRequest{})
+
+			Expect(err).To(MatchBackendError(errs.ErrNoTeam))
+		})
+
+		Specify("Owner can't leave", func() {
+			_, err := teamClient.LeaveTeam(user1.Context(), &pb.LeaveTeamRequest{})
+
+			Expect(err).To(MatchBackendError(errs.ErrOwnerCantLeave))
+		})
+
+		Specify("Member can leave", func() {
+			team.AddMember(user2, false)
+			_, err := teamClient.LeaveTeam(user2.Context(), &pb.LeaveTeamRequest{})
+
+			Expect(err).To(BeNil())
+		})
+
+		Specify("Coowner can leave", func() {
+			team.AddMember(user2, true)
+			_, err := teamClient.LeaveTeam(user2.Context(), &pb.LeaveTeamRequest{})
+
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Get Team Info", func() {
+		var user1 User
+		var user2 User
+
+		var team Team
+
+		BeforeEach(func() {
+			cleanupMongo()
+			user1 = registerUser(authClient, 0)
+			user2 = registerUser(authClient, 1)
+
+			team = createTeam(user1, "Test Team", teamClient)
+		})
+
+		Specify("Can't get team info without a team", func() {
+			info, err := teamClient.GetTeamInfo(user2.Context(), &pb.GetTeamInfoRequest{})
+			Expect(err).To(MatchBackendError(errs.ErrNoTeam))
+			Expect(info).To(BeNil())
+		})
+
+		XSpecify("Join Code is empty if user is a member", func() {
+			team.AddMember(user2, false)
+			info, err := teamClient.GetTeamInfo(user2.Context(), &pb.GetTeamInfoRequest{})
+			Expect(err).To(BeNil())
+			Expect(info).NotTo(BeNil())
+			Expect(info.JoinCode).To(BeNil())
+		})
+
+		XSpecify("Join Code is empty if user is the co-owner", func() {
+			team.AddMember(user2, true)
+			info, err := teamClient.GetTeamInfo(user2.Context(), &pb.GetTeamInfoRequest{})
+			Expect(err).To(BeNil())
+			Expect(info).NotTo(BeNil())
+			Expect(info.JoinCode).To(BeNil())
+		})
+
+		Specify("Join Code not is empty if user is the owner", func() {
+			info, err := teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+			Expect(err).To(BeNil())
+			Expect(info).NotTo(BeNil())
+			Expect(info.JoinCode).NotTo(BeNil())
+		})
+
+		Specify("Member list changes", func() {
+			info, err := teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+			Expect(err).To(BeNil())
+			Expect(info).NotTo(BeNil())
+			Expect(info.Members).NotTo(BeNil())
+			Expect(info.Members).To(HaveLen(1))
+
+			team.AddMember(user2, false)
+
+			info, err = teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+			Expect(err).To(BeNil())
+			Expect(info).NotTo(BeNil())
+			Expect(info.Members).NotTo(BeNil())
+			Expect(info.Members).To(HaveLen(2))
+		})
+	})
+
+	Describe("Update Team", func() {
+		var user1 User
+		var user2 User
+
+		var team Team
+
+		BeforeEach(func() {
+			cleanupMongo()
+			user1 = registerUser(authClient, 0)
+			user2 = registerUser(authClient, 1)
+
+			team = createTeam(user1, "Test Team", teamClient)
+		})
+
+		Specify("Change team name", func() {
+			By("Get initial name")
+			info, err := teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+			Expect(err).To(BeNil())
+			Expect(info).NotTo(BeNil())
+			Expect(info.Name).To(Equal("Test Team"))
+
+			By("Update name")
+			_, err = teamClient.UpdateTeam(user1.Context(), &pb.UpdateTeamRequest{
+				Name: "New test name",
+			})
+
+			Expect(err).To(BeNil())
+
+			By("Get new name")
+			info, err = teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+			Expect(err).To(BeNil())
+			Expect(info).NotTo(BeNil())
+			Expect(info.Name).To(Equal("New test name"))
+		})
+
+		Specify("No team", func() {
+			_, err = teamClient.UpdateTeam(user2.Context(), &pb.UpdateTeamRequest{
+				Name: "New test name",
+			})
+			Expect(err).To(MatchBackendError(errs.ErrNoTeam))
+		})
+
+		Specify("Member cannot change name", func() {
+			team.AddMember(user2, false)
+			_, err = teamClient.UpdateTeam(user2.Context(), &pb.UpdateTeamRequest{
+				Name: "New test name",
+			})
+			Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+		})
+
+		Specify("Co-owner cannot change name", func() {
+			team.AddMember(user2, true)
+			_, err = teamClient.UpdateTeam(user2.Context(), &pb.UpdateTeamRequest{
+				Name: "New test name",
+			})
+			Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+		})
+
+		Specify("Team name already exists", func() {
+			createTeam(user2, "Test Team 2", teamClient)
+
+			_, err = teamClient.UpdateTeam(user1.Context(), &pb.UpdateTeamRequest{
+				Name: "Test Team 2",
+			})
+			Expect(err).To(MatchBackendError(errs.ErrTeamNameTaken))
+		})
+	})
+
+	Describe("Disband Team", func() {
+		var user1 User
+		var user2 User
+
+		var team Team
+
+		BeforeEach(func() {
+			cleanupMongo()
+			user1 = registerUser(authClient, 0)
+			user2 = registerUser(authClient, 1)
+
+			team = createTeam(user1, "Test Team", teamClient)
+		})
+
+		Specify("Cannot disband team if there is no team", func() {
+			_, err := teamClient.DisbandTeam(user2.Context(), &pb.DisbandTeamRequest{})
+			Expect(err).To(MatchBackendError(errs.ErrNoTeam))
+		})
+
+		Specify("Member can't disband team", func() {
+			team.AddMember(user2, false)
+			_, err := teamClient.DisbandTeam(user2.Context(), &pb.DisbandTeamRequest{})
+			Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+		})
+
+		Specify("Co-owner can't disband team", func() {
+			team.AddMember(user2, true)
+			_, err := teamClient.DisbandTeam(user2.Context(), &pb.DisbandTeamRequest{})
+			Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+		})
+
+		Specify("Cannot disband non-empty team", func() {
+			team.AddMember(user2, false)
+			_, err := teamClient.DisbandTeam(user1.Context(), &pb.DisbandTeamRequest{})
+			Expect(err).To(MatchBackendError(errs.ErrDisbandNonEmptyTeam))
+		})
+
+		Specify("Success", func() {
+			_, err := teamClient.DisbandTeam(user1.Context(), &pb.DisbandTeamRequest{})
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Change Lock", func() {
+		var user1 User
+		var user2 User
+
+		var team Team
+
+		BeforeEach(func() {
+			cleanupMongo()
+			user1 = registerUser(authClient, 0)
+			user2 = registerUser(authClient, 1)
+
+			team = createTeam(user1, "Test Team", teamClient)
+		})
+
+		Specify("No team", func() {
+			_, err := teamClient.ChangeLock(user2.Context(), &pb.ChangeLockRequest{
+				ShouldLock: true,
+			})
+			Expect(err).To(MatchBackendError(errs.ErrNoTeam))
+		})
+
+		Specify("Member can't lock team", func() {
+			team.AddMember(user2, false)
+			_, err := teamClient.ChangeLock(user2.Context(), &pb.ChangeLockRequest{
+				ShouldLock: true,
+			})
+			Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+		})
+
+		Specify("Co-owner can't lock team", func() {
+			team.AddMember(user2, true)
+			_, err := teamClient.ChangeLock(user2.Context(), &pb.ChangeLockRequest{
+				ShouldLock: true,
+			})
+			Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+		})
+
+		Specify("Owner can lock team", func() {
+			_, err := teamClient.ChangeLock(user1.Context(), &pb.ChangeLockRequest{
+				ShouldLock: true,
+			})
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Change Co-owner Status", func() {
+		var user1 User
+		var user2 User
+		var user3 User
+
+		BeforeEach(func() {
+			cleanupMongo()
+			user1 = registerUser(authClient, 0)
+			user2 = registerUser(authClient, 1)
+		})
+
+		Context("No team", func() {
+			Specify("Cannot change co-owner status without a team", func() {
+				_, err := teamClient.ChangeCoOwnerStatus(user1.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user2.UserID(),
+					ShouldCoowner: true,
+				})
+				Expect(err).To(MatchBackendError(errs.ErrNoTeam))
+			})
+		})
+
+		Context("Has team", func() {
+			BeforeEach(func() {
+				user3 = registerUser(authClient, 2)
+
+				team := createTeam(user1, "Test Team", teamClient)
+				team.AddMember(user2, false)
+				team.AddMember(user3, false)
+			})
+
+			Specify("Member can't change status", func() {
+				_, err := teamClient.ChangeCoOwnerStatus(user2.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user3.UserID(),
+					ShouldCoowner: true,
+				})
+
+				Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+			})
+
+			Specify("Co-owner can't change status", func() {
+				_, err := teamClient.ChangeCoOwnerStatus(user1.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user2.UserID(),
+				})
+
+				Expect(err).To(BeNil())
+
+				_, err = teamClient.ChangeCoOwnerStatus(user2.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user3.UserID(),
+					ShouldCoowner: true,
+				})
+
+				Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+			})
+
+			Specify("Change member to co-owner and back", func() {
+				By("Get initial info")
+				info, err := teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+				Expect(err).To(BeNil())
+				Expect(info).NotTo(BeNil())
+				Expect(info.Members).NotTo(BeNil())
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user2.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_MEMBER,
+				}))
+
+				By("Change status")
+				_, err = teamClient.ChangeCoOwnerStatus(user1.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user2.UserID(),
+					ShouldCoowner: true,
+				})
+				Expect(err).To(BeNil())
+
+				By("Get new info")
+				info, err = teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+				Expect(err).To(BeNil())
+				Expect(info).NotTo(BeNil())
+				Expect(info.Members).NotTo(BeNil())
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user2.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_COOWNER,
+				}))
+
+
+				By("Change status")
+				_, err = teamClient.ChangeCoOwnerStatus(user1.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user2.UserID(),
+					ShouldCoowner: false,
+				})
+				Expect(err).To(BeNil())
+
+				By("Get third info")
+				info, err = teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+				Expect(err).To(BeNil())
+				Expect(info).NotTo(BeNil())
+				Expect(info.Members).NotTo(BeNil())
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user2.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_MEMBER,
+				}))
+			})
+
+			Specify("No two co-owner", func() {
+				By("Get initial info")
+				info, err := teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+				Expect(err).To(BeNil())
+				Expect(info).NotTo(BeNil())
+				Expect(info.Members).NotTo(BeNil())
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user2.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_MEMBER,
+				}))
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user3.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_MEMBER,
+				}))
+
+				By("Set user2 to the co-owner")
+				_, err = teamClient.ChangeCoOwnerStatus(user1.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user2.UserID(),
+					ShouldCoowner: true,
+				})
+				Expect(err).To(BeNil())
+
+				By("Get second info")
+				info, err = teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+				Expect(err).To(BeNil())
+				Expect(info).NotTo(BeNil())
+				Expect(info.Members).NotTo(BeNil())
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user2.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_COOWNER,
+				}))
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user3.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_MEMBER,
+				}))
+
+				By("Set user3 to the co-owner")
+				_, err = teamClient.ChangeCoOwnerStatus(user1.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user3.UserID(),
+					ShouldCoowner: true,
+				})
+				Expect(err).To(BeNil())
+
+				By("Get second info")
+				info, err = teamClient.GetTeamInfo(user1.Context(), &pb.GetTeamInfoRequest{})
+
+				Expect(err).To(BeNil())
+				Expect(info).NotTo(BeNil())
+				Expect(info.Members).NotTo(BeNil())
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user2.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_MEMBER,
+				}))
+				Expect(info.Members).To(ContainElement(&pb.GetTeamInfoResponse_Member{
+					ID: user3.UserID(),
+					Name: "test",
+					Class: 0,
+					Rank: pb.GetTeamInfoResponse_Member_k_COOWNER,
+				}))
+			})
+
+			XSpecify("Cannot change owner co-owner status", func() {
+				_, err := teamClient.ChangeCoOwnerStatus(user1.Context(), &pb.ChangeCoOwnerStatusRequest{
+					UserId: user1.UserID(),
+					ShouldCoowner: true,
+				})
+
+				Expect(err).To(MatchBackendError(errs.ErrNotAuthorized))
+			})
+		})
+	})
+
 	Describe("Kick User", func() {
 		var user1 User
 		var user2 User
