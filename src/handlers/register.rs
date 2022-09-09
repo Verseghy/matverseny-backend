b8e120ae5e324688376d2bc2534b15695776081e
Szepesi Tibor <sztibi023@gmail.com>
2022-09-09 21:17:41 +0200

    Refactor register handler

--- src/handlers/register.rs
+++ src/handlers/register.rs
@@ -1,24 +1,26 @@
-use crate::{Json, Result, SharedTrait};
+use crate::{iam::Claims, Json, Result, SharedTrait};
 use axum::{http::StatusCode, Extension};
 use entity::users::{self, Class};
 use sea_orm::{EntityTrait, Set};
 use serde::Deserialize;
+use std::sync::Arc;
 
 #[derive(Deserialize)]
 pub struct Request {
-    pub id: String,
     pub school: String,
     pub class: Class,
 }
 
 pub async fn register<S: SharedTrait>(
     Extension(shared): Extension<S>,
+    Extension(claims): Extension<Arc<Claims>>,
     Json(request): Json<Request>,
 ) -> Result<StatusCode> {
     let user = users::ActiveModel {
-        id: Set(request.id),
+        id: Set(claims.subject.clone()),
         school: Set(request.school),
         class: Set(request.class),
+        ..Default::default()
     };
 
     users::Entity::insert(user).exec(shared.db()).await?;
