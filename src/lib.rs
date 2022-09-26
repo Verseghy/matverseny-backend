99cb6e2e8bc4684ff9099f5059f3a778d8ca9422
Szepesi Tibor <sztibi023@gmail.com>
2022-09-26 14:53:45 +0200

    Librarify

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+pub mod error;
+mod handlers;
+mod iam;
+mod json;
+mod middlewares;
+mod shared;
+mod utils;
+
+use error::{Error, Result};
+use json::*;
+use shared::*;
+
+use axum::{http::header::AUTHORIZATION, Router};
+use std::{iter::once, net::TcpListener};
+use tokio::signal;
+use tower::ServiceBuilder;
+use tower_http::{
+    cors::{Any, CorsLayer},
+    ServiceBuilderExt,
+};
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install SIGTERM handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    };
+}
+
+fn app<S: SharedTrait>(shared: S) -> Router {
+    let cors_layer = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    let middlewares = ServiceBuilder::new()
+        .catch_panic()
+        .sensitive_headers(once(AUTHORIZATION))
+        .propagate_x_request_id()
+        .add_extension(shared)
+        .layer(middlewares::GetClaimsLayer::<S>::new())
+        .compression()
+        .decompression()
+        .layer(cors_layer)
+        .into_inner();
+
+    handlers::routes::<S>().layer(middlewares)
+}
+
+pub async fn run(listener: TcpListener) {
+    let shared = Shared::new().await;
+
+    tracing::info!(
+        "listening on port {}",
+        listener.local_addr().unwrap().port()
+    );
+
+    axum::Server::from_tcp(listener)
+        .expect("failed to start server")
+        .serve(app(shared).into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,6 @@
-mod error;
-mod handlers;
-mod iam;
-mod json;
-mod middlewares;
-mod shared;
-mod utils;
-
-use error::{Error, Result};
-use json::*;
-use shared::*;
-
-use axum::{http::header::AUTHORIZATION, Router};
 use dotenvy::dotenv;
-use std::{
-    iter::once,
-    net::{Ipv4Addr, SocketAddr},
-};
-use tokio::signal;
-use tower::ServiceBuilder;
-use tower_http::{
-    cors::{Any, CorsLayer},
-    ServiceBuilderExt,
-};
+use std::net::{Ipv4Addr, SocketAddr, TcpListener};
 use tracing::level_filters::LevelFilter;
-
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install SIGTERM handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    };
-}
-
-fn app<S: SharedTrait>(shared: S) -> Router {
-    let cors_layer = CorsLayer::new()
-        .allow_origin(Any)
-        .allow_methods(Any)
-        .allow_headers(Any);
-
-    let middlewares = ServiceBuilder::new()
-        .catch_panic()
-        .sensitive_headers(once(AUTHORIZATION))
-        .propagate_x_request_id()
-        .add_extension(shared)
-        .layer(middlewares::GetClaimsLayer::<S>::new())
-        .compression()
-        .decompression()
-        .layer(cors_layer)
-        .into_inner();
-
-    handlers::routes::<S>().layer(middlewares)
-}
 
 #[tokio::main]
 async fn main() {
@@ -78,12 +12,8 @@ async fn main() {
         .init();
 
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3002));
-    let shared = Shared::new().await;
 
-    tracing::info!("listening on port {}", addr.port());
-    axum::Server::bind(&addr)
-        .serve(app(shared).into_make_service())
-        .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap()
+    let listener = TcpListener::bind(addr).expect("failed to bind tcp listener");
+
+    matverseny_backend::run(listener).await;
 }
