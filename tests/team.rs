2f3f627762cebf60f7dd63f18ea65de3daa87be5
Szepesi Tibor <sztibi023@gmail.com>
2022-09-26 20:18:09 +0200

    Add tests for team creation

---
+++ tests/team.rs
@@ -0,0 +1,78 @@
+mod utils;
+
+use http::StatusCode;
+use matverseny_backend::error;
+use serde_json::{json, Value};
+use utils::{assert_error, App};
+
+#[tokio::test]
+async fn success() {
+    let app = App::new().await;
+    let user = app.register_user().await;
+
+    let res = app
+        .post("/team/create")
+        .user(&user)
+        .json(&json!({
+            "name": "Test Team",
+        }))
+        .send()
+        .await;
+
+    assert_eq!(res.status(), StatusCode::CREATED);
+
+    let body = res.json::<Value>().await;
+
+    assert!(body.is_object());
+    assert!(body["id"].is_string());
+}
+
+#[tokio::test]
+async fn name_already_taken() {
+    let app = App::new().await;
+    let user = app.register_user().await;
+
+    let res = app
+        .post("/team/create")
+        .user(&user)
+        .json(&json!({
+            "name": "Test Team",
+        }))
+        .send()
+        .await;
+
+    assert_eq!(res.status(), StatusCode::CREATED);
+
+    let body = res.json::<Value>().await;
+
+    assert!(body.is_object());
+    assert!(body["id"].is_string());
+
+    let res = app
+        .post("/team/create")
+        .user(&user)
+        .json(&json!({
+            "name": "Test Team",
+        }))
+        .send()
+        .await;
+
+    assert_error!(res, error::DUPLICATE_TEAM_NAME);
+}
+
+#[tokio::test]
+async fn not_registered() {
+    let app = App::new().await;
+    let user = utils::iam::register_user().await;
+
+    let res = app
+        .post("/team/create")
+        .user(&user)
+        .json(&json!({
+            "name": "Test Team",
+        }))
+        .send()
+        .await;
+
+    assert_error!(res, error::USER_NOT_REGISTERED)
+}
