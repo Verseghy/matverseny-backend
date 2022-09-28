f204ae39e54d37adf1877597e2dc48104be8ee7a
Szepesi Tibor <sztibi023@gmail.com>
2022-09-29 00:25:39 +0200

    Fix create team tests

--- tests/team.rs
+++ tests/team.rs
@@ -23,9 +23,43 @@ async fn success() {
     assert!(body.is_object());
     assert!(body["id"].is_string());
 }
-
 #[tokio::test]
 async fn name_already_taken() {
+    let app = App::new().await;
+    let user = app.register_user().await;
+
+    let res = app
+        .post("/team/create")
+        .user(&user)
+        .json(&json!({
+            "name": "Test Team",
+        }))
+        .send()
+        .await;
+
+    assert_eq!(res.status(), StatusCode::CREATED);
+
+    let body = res.json::<Value>().await;
+
+    assert!(body.is_object());
+    assert!(body["id"].is_string());
+
+    let user2 = app.register_user().await;
+
+    let res = app
+        .post("/team/create")
+        .user(&user2)
+        .json(&json!({
+            "name": "Test Team",
+        }))
+        .send()
+        .await;
+
+    assert_error!(res, error::DUPLICATE_TEAM_NAME);
+}
+
+#[tokio::test]
+async fn already_in_team() {
     let app = App::new().await;
     let user = app.register_user().await;
 
@@ -54,7 +88,7 @@ async fn name_already_taken() {
         .send()
         .await;
 
-    assert_error!(res, error::DUPLICATE_TEAM_NAME);
+    assert_error!(res, error::ALREADY_IN_TEAM);
 }
 
 #[tokio::test]
