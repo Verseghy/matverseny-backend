b02841e009c9221c0cf01b5bd68d7552e57dfe7e
Szepesi Tibor <sztibi023@gmail.com>
2022-12-05 15:18:14 +0100

    Rework errors to support websocket errors

--- src/error/constants.rs
+++ src/error/constants.rs
@@ -1,67 +1,102 @@
 use super::const_error;
 
-const_error!(INTERNAL, INTERNAL_SERVER_ERROR, 0, "internal server error");
-const_error!(
-    JSON_MISSING_FIELDS,
-    UNPROCESSABLE_ENTITY,
-    1,
-    "missing fields"
-);
-const_error!(JSON_SYNTAX_ERROR, BAD_REQUEST, 2, "syntax error");
-const_error!(
-    JSON_CONTENT_TYPE,
-    BAD_REQUEST,
-    3,
-    "missing or wrong content-type"
-);
-const_error!(JSON_VALIDATE_INVALID, BAD_REQUEST, 4, "invalid data");
-const_error!(DUPLICATE_TEAM_NAME, BAD_REQUEST, 5, "team name exists");
-const_error!(JOIN_CODE_NOT_FOUND, BAD_REQUEST, 6, "join code not found");
-const_error!(LOCKED_TEAM, BAD_REQUEST, 7, "locked team");
-const_error!(ALREADY_IN_TEAM, BAD_REQUEST, 8, "already in team");
-const_error!(
-    COULD_NOT_GET_CLAIMS,
-    UNAUTHORIZED,
-    9,
-    "could not get claims"
-);
-const_error!(USER_ALREADY_EXISTS, BAD_REQUEST, 10, "user already exists");
-const_error!(USER_NOT_IN_TEAM, BAD_REQUEST, 11, "user is not in a team");
-const_error!(USER_NOT_REGISTERED, FORBIDDEN, 12, "user is not registered");
-const_error!(
-    USER_NOT_OWNER,
-    FORBIDDEN,
-    13,
-    "user must be the owner of the team"
-);
-const_error!(
-    NO_SUCH_MEMBER,
-    BAD_REQUEST,
-    14,
-    "user is not a member of the team"
-);
-const_error!(
-    USER_NOT_COOWNER,
-    FORBIDDEN,
-    15,
-    "user must be the owner or the coowner of the team"
-);
-const_error!(
-    CANNOT_KICK_OWNER,
-    FORBIDDEN,
-    16,
-    "cannot kick the owner of a team"
-);
-const_error!(CANNOT_KICK_THEMSELF, FORBIDDEN, 17, "cannot kick yourself");
-const_error!(
-    FAILED_TO_GENERATE_JOIN_CODE,
-    INTERNAL_SERVER_ERROR,
-    18,
-    "failed to generate join code"
-);
-const_error!(
-    OWNER_CANNOT_LEAVE,
-    FORBIDDEN,
-    19,
-    "the owner cannot leave the team"
-);
+const_error! {
+    #[error(0, "internal server error")]
+    #[status(INTERNAL_SERVER_ERROR)]
+    const INTERNAL;
+}
+const_error! {
+    #[error(1, "missing fields")]
+    #[status(UNPROCESSABLE_ENTITY)]
+    const JSON_MISSING_FIELDS;
+}
+const_error! {
+    #[error(2, "syntax error")]
+    #[status(BAD_REQUEST)]
+    const JSON_SYNTAX_ERROR;
+}
+const_error! {
+    #[error(3, "missing or wrong content-type")]
+    #[status(BAD_REQUEST)]
+    const JSON_CONTENT_TYPE;
+}
+const_error! {
+    #[error(4, "invalid data")]
+    #[status(BAD_REQUEST)]
+    const JSON_VALIDATE_INVALID;
+}
+const_error! {
+    #[error(5, "team name exists")]
+    #[status(BAD_REQUEST)]
+    const DUPLICATE_TEAM_NAME;
+}
+const_error! {
+    #[error(6, "join code not found")]
+    #[status(BAD_REQUEST)]
+    const JOIN_CODE_NOT_FOUND;
+}
+const_error! {
+    #[error(7, "locked team")]
+    #[status(BAD_REQUEST)]
+    const LOCKED_TEAM;
+}
+const_error! {
+    #[error(8, "already in team")]
+    #[status(BAD_REQUEST)]
+    const ALREADY_IN_TEAM;
+}
+const_error! {
+    #[error(9, "could not get claims")]
+    #[status(UNAUTHORIZED)]
+    const COULD_NOT_GET_CLAIMS;
+}
+const_error! {
+    #[error(10, "user already exists")]
+    #[status(BAD_REQUEST)]
+    const USER_ALREADY_EXISTS;
+}
+const_error! {
+    #[error(11, "user is not in a team")]
+    #[status(BAD_REQUEST)]
+    const USER_NOT_IN_TEAM;
+}
+const_error! {
+    #[error(12, "user is not registered")]
+    #[status(FORBIDDEN)]
+    const USER_NOT_REGISTERED;
+}
+const_error! {
+    #[error(13, "user must be the owner of the team")]
+    #[status(FORBIDDEN)]
+    const USER_NOT_OWNER;
+}
+const_error! {
+    #[error(14, "user is not a member of the team")]
+    #[status(BAD_REQUEST)]
+    const NO_SUCH_MEMBER;
+}
+const_error! {
+    #[error(15, "user must be the owner or the co-owner of the team")]
+    #[status(FORBIDDEN)]
+    const USER_NOT_COOWNER;
+}
+const_error! {
+    #[error(16, "cannot kick the owner of a team")]
+    #[status(FORBIDDEN)]
+    const CANNOT_KICK_OWNER;
+}
+const_error! {
+    #[error(17, "cannot kick yourself")]
+    #[status(FORBIDDEN)]
+    const CANNOT_KICK_THEMSELF;
+}
+const_error! {
+    #[error(18, "failed to generate join code")]
+    #[status(INTERNAL_SERVER_ERROR)]
+    const FAILED_TO_GENERATE_JOIN_CODE;
+}
+const_error! {
+    #[error(19, "the owner cannot leave the team")]
+    #[status(FORBIDDEN)]
+    const OWNER_CANNOT_LEAVE;
+}
--- src/error/mod.rs
+++ src/error/mod.rs
@@ -4,33 +4,34 @@ mod db;
 pub use constants::*;
 pub use db::*;
 
-use crate::json::Json;
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use bytes::{BufMut, Bytes, BytesMut};
 use sea_orm::DbErr;
 use serde_json::json;
 
 #[derive(Debug)]
-pub struct Error {
-    status: StatusCode,
+pub struct ErrorInner<'a> {
+    status: Option<StatusCode>,
     code: u32,
-    message: &'static str,
+    message: &'a str,
 }
 
+pub type Error = ErrorInner<'static>;
 pub type Result<T> = std::result::Result<T, Error>;
 
-impl Error {
+impl<'a> ErrorInner<'a> {
     #[inline]
     pub fn internal<E: Into<Box<dyn std::error::Error>>>(error: E) -> Self {
-        tracing::error!("internal error: {}", error.into());
+        error!("internal error: {}", error.into());
         constants::INTERNAL
     }
 
     #[inline]
-    const fn new(status: StatusCode, code: u32, message: &'static str) -> Self {
-        Error {
+    const fn new(status: Option<StatusCode>, code: u32, message: &'a str) -> ErrorInner<'a> {
+        ErrorInner {
             status,
             code,
             message,
@@ -43,36 +44,70 @@ impl Error {
     }
 
     #[inline]
-    pub const fn status(&self) -> StatusCode {
+    pub const fn status(&self) -> Option<StatusCode> {
         self.status
     }
-}
 
-impl IntoResponse for Error {
     #[inline]
-    fn into_response(self) -> Response {
-        (
-            self.status,
-            Json(json!({
-                "code": self.code,
-                "error": self.message,
-            })),
+    pub const fn message(&self) -> &str {
+        self.message
+    }
+
+    pub fn to_bytes(&self) -> Bytes {
+        let mut buf = BytesMut::with_capacity(128).writer();
+
+        serde_json::to_writer(
+            &mut buf,
+            &json!({
+                "code": self.code(),
+                "error": self.message(),
+            }),
         )
-            .into_response()
+        .expect("failed to serialize error");
+
+        buf.into_inner().freeze()
     }
 }
 
-impl From<DbErr> for Error {
+impl IntoResponse for ErrorInner<'_> {
+    #[inline]
+    fn into_response(self) -> Response {
+        let Some(status) = self.status else {
+            panic!("cannot convert an error without status to a response")
+        };
+
+        (status, self.to_bytes()).into_response()
+    }
+}
+
+impl From<DbErr> for ErrorInner<'_> {
     #[inline]
     fn from(error: DbErr) -> Self {
         Error::internal(error)
     }
 }
 
+impl From<axum::Error> for ErrorInner<'_> {
+    #[inline]
+    fn from(error: axum::Error) -> Self {
+        Error::internal(error)
+    }
+}
+
 macro_rules! const_error {
-    ($name:ident, $status:ident, $code:literal, $msg:literal) => {
+    (
+        #[error($code:literal, $msg:literal)]
+        $(#[status($status:ident)])?
+        const $name:ident;
+    ) => {
         pub const $name: $crate::error::Error =
-            $crate::error::Error::new(::axum::http::StatusCode::$status, $code, $msg);
+            $crate::error::Error::new($crate::error::const_error!(@status $($status)?), $code, $msg);
+    };
+    (@status $status:ident) => {
+        Some(::axum::http::StatusCode::$status)
+    };
+    (@status) => {
+        None
     };
 }
 
