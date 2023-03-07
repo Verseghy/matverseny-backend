9188c5ae1d61458aed13459c63222daebc848ce0
Zoltán Szepesi <2dcube01@gmail.com>
2023-03-07 05:30:22 +0100

    Make k8s resources kinda HA

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -8,5 +8,8 @@ kubectl apply -f namespace-matverseny-backend.yaml
 kubectl apply -f ./matverseny-backend/ingress.yaml
 kubectl apply -f ./matverseny-backend/kafka-1.yaml
 kubectl apply -f ./matverseny-backend/postgres-1.yaml
+kubectl apply -f ./matverseny-backend/secret.yaml
+echo please fill in the secret for IAM_APP_SECRET
+sleep 120
 kubectl apply -f ./matverseny-backend/matverseny-backend.yaml
 kubectl apply -f ./matverseny-backend/matverseny-backend-migration.yaml
