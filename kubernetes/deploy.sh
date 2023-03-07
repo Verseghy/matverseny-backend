233b0acc0dd4be0e5b7257e241d483b67b879c5b
Zoltán Szepesi <2dcube01@gmail.com>
2023-03-07 02:54:21 +0100

    Make k8s resources kinda HA

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
+
 ./operators.sh
 sleep 10
 kubectl apply -f cluster-issuer.yaml
 kubectl apply -f configmap-ingress-nginx.yaml
 kubectl apply -f namespace-matverseny-backend.yaml
-kubectl apply -f ./matverseny-backend
+kubectl apply -f ./matverseny-backend/ingress.yaml
+kubectl apply -f ./matverseny-backend/kafka-1.yaml
+kubectl apply -f ./matverseny-backend/postgres-1.yaml
+kubectl apply -f ./matverseny-backend/matverseny-backend.yaml
+kubectl apply -f ./matverseny-backend/matverseny-backend-migration.yaml
