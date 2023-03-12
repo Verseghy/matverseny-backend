07c2927f71d6a473c7bea9a24238e1771cfb4303
Szepesi Tibor <sztibi023@gmail.com>
2023-03-12 21:58:32 +0100

    Add create_admin_user command

---
+++ cmds/src/create_admin_user.rs
@@ -0,0 +1,26 @@
+use dotenvy::dotenv;
+use libiam::{
+    testing::{actions::assign_action_to_user, Database},
+    Iam, User,
+};
+use std::env::{self, args};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    dotenv().ok();
+
+    let email = args().nth(1).expect("no email given");
+    let iam = Iam::new(&env::var("IAM_URL").expect("IAM_URL is not set"));
+    let database = Database::connect("mysql://iam:secret@localhost:3306/iam").await;
+
+    let id = User::register(&iam, "Admin User", &email, "test")
+        .await
+        .unwrap();
+
+    assign_action_to_user(&database, "mathcompetition.problems", &id.to_string()).await;
+    assign_action_to_user(&database, "mathcompetition.admin", &id.to_string()).await;
+
+    let user = User::login(&iam, &email, "test").await.unwrap();
+
+    println!("{}", user.token());
+}
