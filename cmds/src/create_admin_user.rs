0140c0914e7e6c1e93b58e366830f2b9564b1031
TwoDCube <2dcube01@gmail.com>
2023-11-26 13:46:28 +0100

    Try to login before creating new user, ensure actions are there

--- cmds/src/create_admin_user.rs
+++ cmds/src/create_admin_user.rs
@@ -4,6 +4,14 @@ use libiam::{
     Iam, User,
 };
 use std::env::{self, args};
+use jsonwebtoken::{Algorithm, DecodingKey, Validation};
+use libiam::testing::actions::ensure_action;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -13,10 +21,27 @@ async fn main() {
     let iam = Iam::new(&env::var("IAM_URL").expect("IAM_URL is not set"));
     let database = Database::connect("mysql://iam:secret@localhost:3306/iam").await;
 
-    let id = User::register(&iam, "Admin User", &email, "test")
-        .await
-        .unwrap();
+    let id = 'id: {
+        if let Ok(user) = User::login(&iam, &email, "test").await {
+            break 'id jsonwebtoken::decode::<Claims>(
+                user.token(),
+                &DecodingKey::from_secret(&[]),
+                &{
+                    let mut v = Validation::new(Algorithm::RS256);
+                    v.insecure_disable_signature_validation();
+                    v.set_audience(&["https://verseghy-gimnazium.net"]);
+                    v
+                }
+            ).unwrap().claims.sub;
+        }
 
+        User::register(&iam, "Admin User", &email, "test")
+            .await
+            .unwrap().to_string()
+    };
+
+    ensure_action(&database, "mathcompetition.problems", false).await;
+    ensure_action(&database, "mathcompetition.admin", false).await;
     assign_action_to_user(&database, "mathcompetition.problems", &id.to_string()).await;
     assign_action_to_user(&database, "mathcompetition.admin", &id.to_string()).await;
 
