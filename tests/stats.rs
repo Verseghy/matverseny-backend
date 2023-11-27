1446a88b220636ce8c5a3c312ce9523aecb9d559
Szepesi Tibor <sztibi023@gmail.com>
2023-11-27 22:14:57 +0100

    Add test

--- tests/stats.rs
+++ tests/stats.rs
@@ -117,3 +117,105 @@ async fn works() {
         }])
     );
 }
+
+#[tokio::test]
+#[serial]
+async fn works_after_delete() {
+    let app = get_cached_app().await;
+    app.clean_database().await;
+
+    let admin_user = utils::iam::register_user().await;
+    utils::iam::make_admin(&admin_user).await;
+
+    // Setup
+    let res = app
+        .post("/problem")
+        .user(&admin_user)
+        .json(&json!({
+            "body": "some body",
+            "solution": 23,
+            "image": "image link",
+        }))
+        .send()
+        .await;
+
+    assert_eq!(res.status(), StatusCode::CREATED);
+
+    let body: Value = res.json().await;
+    let id = body.get("id").unwrap();
+
+    let owner = app.register_user().await;
+    let team = app.create_team(&owner).await;
+
+    team.lock().await;
+
+    let res = app
+        .post("/competition/solution")
+        .user(&owner)
+        .json(&json!({
+            "problem": id,
+            "solution": 23,
+        }))
+        .send()
+        .await;
+
+    assert!(res.status().is_success());
+
+    let time = Utc::now().to_rfc3339();
+
+    let res = app
+        .post("/competition/solution")
+        .user(&owner)
+        .json(&json!({
+            "problem": id,
+            "solution": null,
+        }))
+        .send()
+        .await;
+
+    assert!(res.status().is_success());
+
+    let time2 = Utc::now().to_rfc3339();
+
+    let res = app
+        .post("/stats")
+        .user(&admin_user)
+        .json(&json!({
+            "timestamp": time,
+        }))
+        .send()
+        .await;
+
+    assert_eq!(res.status(), StatusCode::OK);
+
+    let body: Value = res.json().await;
+
+    assert_json_include!(
+        actual: body,
+        expected: json!([{
+            "correct": 1,
+            "wrong": 0,
+        }])
+    );
+
+    let res = app
+        .post("/stats")
+        .user(&admin_user)
+        .json(&json!({
+            "timestamp": time2,
+        }))
+        .send()
+        .await;
+
+    assert_eq!(res.status(), StatusCode::OK);
+
+    let body: Value = res.json().await;
+
+    assert_json_include!(
+        actual: body,
+        expected: json!([{
+            "correct": 0,
+            "wrong": 0,
+        }])
+    );
+}
